89990018c087cfd62a1725c828ad86b2a5025931
Vadim Ignatov <ignatov0131@gmail.com>
2021-02-02 15:21:47 +0300

    Getting neccessery timetable and output in console

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '1608798236:AAHDzQWo8Qw9qAmKBdSkSEDjl2mz-NKdDKY';
-const bot = new TelegramBot(token, { polling: true });
+const TelegramBot = require('node-telegram-bot-api');               // Подключение модуля Telegram
+const token = '1608798236:AAHDzQWo8Qw9qAmKBdSkSEDjl2mz-NKdDKY';     // Токен
+const bot = new TelegramBot(token, { polling: true });              // Экземпляр модуля
 
-const application = require('./src/js/app-data.js'); // Данные для приложения
-const keyboards = require('./src/js/keyboards.js'); // Клавиатуры
+const application = require('./src/js/app-data.js');                // Данные для приложения
+const keyboards = require('./src/js/keyboards.js');                 // Клавиатуры
 
-const MongoClient = require('mongodb').MongoClient;
-const dbName = "schedule";
-const urlConnect = 'mongodb://localhost:27017/';
-
-MongoClient.connect(urlConnect, (error, client) => {
-  if (error) {
-    return console.log(error);
-  } else {
-    console.log("Connect successfully to server!")
-  }
-
-  const db = client.db(dbName);
-
-  db.collection('groups').findOne({groupName: "БСТ1902"}, (err, results) => {
-    const scheduleDay = results.odd.mon[0];
-
-    for (let key in scheduleDay) {
-      console.log(scheduleDay[key]);
-    }
-  });
-
-  client.close();
-})
+const MongoClient = require('mongodb').MongoClient;                 // Связь с клиентом MongoDb
+const dbName = "schedule";                                          // Имя БД
+const urlConnect = 'mongodb://localhost:27017/';                    // Адрес подключения
 
 
 /* Commands */
@@ -83,6 +63,8 @@ const schedule = {
       const weekday = application.calendar.weekday[date.getDay()];
       const month = application.calendar.month[date.getMonth()];
 
+      getTimetable(date);
+
       bot.sendMessage(id, `
 \u{1F4C6} Расписание на сегодня (${day} ${month}, ${weekday}) для группы БСТ1902
 ------------------------------------------------------------
@@ -105,6 +87,8 @@ const schedule = {
       const day = tomorrowDate.getDate();
       const weekday = application.calendar.weekday[tomorrowDate.getDay()];
       const month = application.calendar.month[tomorrowDate.getMonth()];
+
+      getTimetable(tomorrowDate);
 
       bot.sendMessage(id, `
 Расписание на завтра (${day} ${month}, ${weekday}) для группы БСТ1902 \u{1F4CC}
@@ -134,4 +118,60 @@ function validateDate(date) {
   }
 }
 
+function isEvenWeek(date) {
+  let odd = false;
+
+  const startDate = new Date(date.getFullYear(), 0, 1);
+  const startDay = startDate.getDay();
+
+  let difference = Math.round((date - startDate) / (1000 * 60 * 60 * 24 * 7));
+
+  if (startDay <= 4) {
+    difference++;
+    console.log(difference)
+  }
+
+  if (date.getDay() == 1) {
+    difference++;
+    console.log(difference)
+  }
+
+  difference % 2 ? odd = true : odd = false;
+
+  if (odd) {
+    return "odd"
+  } else {
+    return "even"
+  }
+}
+
+function queryToDB(odd, weekdayName) {
+  let returnArray = [];
+
+  MongoClient.connect(urlConnect, async (error, client) => {
+    if (error) {
+      return console.log(error);
+    } else {
+      console.log("Connect successfully to server!")
+    }
+
+    const db = client.db(dbName);
+
+    db.collection('groups').findOne({ groupName: "БСТ1902" }, (err, results) => {
+      returnArray = JSON.parse(JSON.stringify(results[odd][weekdayName]));
+      console.log(returnArray);
+    })
+
+    client.close();
+  });
+
+  //console.log(returnArray);
+}
+
+function getTimetable(date) {
+  const odd = isEvenWeek(date);
+  const weekdayName = application.weekdayName[date.getDay()];
+
+  queryToDB(odd, weekdayName);
+}
 
