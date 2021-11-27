73c3b993a960feacc275934100f9852cc8e6003e
Vadim Ignatov <ignatov0131@gmail.com>
2021-11-27 19:10:54 +0300

    Add options keyboard

---
+++ src/keyboards/optionKeyboard.ts
@@ -0,0 +1,9 @@
+import { Markup } from 'telegraf';
+import { emoji } from 'node-emoji';
+
+export default Markup.inlineKeyboard([
+  Markup.button.callback(`${emoji.arrows_counterclockwise} Сменить группу`, 'option_changeGroup'),
+  Markup.button.url(
+   `${emoji.love_letter} Оставить фидбэк`, 'google.com'
+  )
+]);
