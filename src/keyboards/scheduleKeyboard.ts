29b96af0c1b534553142c570cb128700e0849567
Vadim Ignatov <ignatov0131@gmail.com>
2021-11-28 20:12:09 +0300

    Upd schedule keyboard

--- src/keyboards/scheduleKeyboard.ts
+++ src/keyboards/scheduleKeyboard.ts
@@ -2,7 +2,6 @@ import { Markup } from 'telegraf';
 import { emoji } from 'node-emoji';
 
 export default Markup.inlineKeyboard([
-  Markup.button.callback(`${emoji.orange_book} Расписание на сегодня`, 'getSchedule_today'),
-  Markup.button.callback(`${emoji.green_book} Расписание на завтра`, `getSchedule_tomorrow`),
-  Markup.button.callback(`${emoji.blue_book} Расписание на текущую неделю`, 'getSchedule_week')
+  Markup.button.callback(`${emoji.orange_book} На сегодня`, 'getSchedule_today'),
+  Markup.button.callback(`${emoji.green_book} На завтра`, `getSchedule_tomorrow`)
 ]);
