1b0d9363012f648273619f0897f6c6402960e9f9
Vadim Ignatov <ignatov0131@gmail.com>
2022-03-06 20:17:58 +0300

    Replace method into utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
+import { Dayjs } from 'dayjs';
+
+/** Interface for custom date functions */
+interface DateWorker {
+  (date: Dayjs): boolean
+}
+
 /**
  * Parse string into
  * @param source - Source string
@@ -6,3 +13,15 @@
 export const parseParameters = (source: string, single = true): string | string[] => {
   return single ? source.split('_')[1] : source.split('_');
 };
+
+/**
+ * Returns true, if input date is holiday
+ * @param dateToCompare - Input date
+ */
+export const isHoliday: DateWorker = (dateToCompare) => dateToCompare.weekday() === 5 || dateToCompare.weekday() === 6;
+
+/**
+ * Returns true, if input date located on even week
+ * @param dateToCompare - Input date
+ */
+export const isEvenWeek: DateWorker = (dateToCompare) => dateToCompare.week() % 2 === 0;
