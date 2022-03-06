19a06904dffd39879362b32cdde52f9e74b37b5c
Vadim Ignatov <ignatov0131@gmail.com>
2022-03-06 19:52:18 +0300

    Rework entry point

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import { Telegraf } from 'telegraf';
+import { CustomContext } from '@/types/global';
+import commands from '@/commands/command';
+import queryProvider from '@/services/queryProvider';
+
+const Bot = new Telegraf<CustomContext>(process.env.TG_TOKEN as string);
+
+/** Bot launching */
+mongoose.connect(process.env.MONGO_URL as string)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .then(() => {
+    /**
+     * Commands registration
+     */
+    commands.forEach(item => {
+      Bot.command(item.name, item.handler);
+    });
+
+    /**
+     * Handler registration for commands
+     */
+    queryProvider.forEach(query => {
+      Bot.action(query.trigger, query.action);
+    });
+
+    return Bot.launch();
+  })
+  .then(() => {
+    console.log('Bot has started');
+  });
