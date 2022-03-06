f7412ec3ccff76d1d98e15a168f21f1caea4efba
Vadim Ignatov <ignatov0131@gmail.com>
2022-03-06 20:52:20 +0300

    Create schedule types

---
+++ src/types/schedule.ts
@@ -0,0 +1,26 @@
+/** Time for schedule getting */
+export type ScheduleTime = 'today' | 'tomorrow';
+
+/** Lesson types */
+type LessonType = 'Lecture' | 'Practice' | 'Laboratory';
+
+/** Lesson for schedule */
+export interface Lesson {
+  /** Serial number */
+  number: number;
+
+  /** Name of lesson */
+  name: string;
+
+  /** Lesson type */
+  type: LessonType;
+
+  /** Lesson time (start and end) */
+  time: string;
+
+  /** Cabinet number where lesson is teach */
+  cabinet: string;
+
+  /** Teacher name */
+  teacher: string;
+}
