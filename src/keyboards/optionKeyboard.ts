b80738421b9853081bd46136287d85f6390257f5
Vadim Ignatov <ignatov0131@gmail.com>
2022-03-06 19:43:34 +0300

    Upd option keyboard

--- src/keyboards/optionKeyboard.ts
+++ src/keyboards/optionKeyboard.ts
@@ -3,7 +3,5 @@ import { emoji } from 'node-emoji';
 
 export default Markup.inlineKeyboard([
   Markup.button.callback(`${emoji.arrows_counterclockwise} Сменить группу`, 'option_changeGroup'),
-  Markup.button.url(
-   `${emoji.love_letter} Оставить фидбэк`, 'google.com'
-  )
+  Markup.button.url(`${emoji.love_letter} Оставить фидбэк`, 'google.com')
 ]);
