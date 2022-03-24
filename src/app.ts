e535b7c348d3cddbcaa454cb12dfb8747083f2d1
Desktop in home (Matvei) <motvig31rus@gmail.com>
2022-03-24 16:15:14 +0300

    Minimize lines in app

--- src/app.ts
+++ src/app.ts
@@ -8,26 +8,14 @@ const Bot = new Telegraf<CustomContext>(process.env.TG_TOKEN as string);
 
 /** Bot launching */
 mongoose.connect(process.env.MONGO_URL as string)
+  .then(() => console.log('MongoDB is connected'))
   .then(() => {
-    console.log('MongoDB is connected');
-  })
-  .then(() => {
-    /**
-     * Commands registration
-     */
-    commands.forEach(item => {
-      Bot.command(item.name, item.handler);
-    });
+    /** Commands registration */
+    commands.forEach(item => Bot.command(item.name, item.handler));
 
-    /**
-     * Handler registration for commands
-     */
-    queryProvider.forEach(query => {
-      Bot.action(query.trigger, query.action);
-    });
+    /** Handler for commands registration */
+    queryProvider.forEach(query => Bot.action(query.trigger, query.action));
 
     return Bot.launch();
   })
-  .then(() => {
-    console.log('Bot has started');
-  });
+  .then(() => console.log('Bot has started'));
